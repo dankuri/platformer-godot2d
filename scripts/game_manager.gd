c844b159ff434f688635cf769b07eb2675338ac1
dankuri <danek.il420@gmail.com>
2025-04-14 03:56:41 +0500

    dynamic total coins counter

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,17 @@
 extends Node
 
 var score := 0
+var total_coins := 0
 
 @onready var score_label: Label = $ScoreLabel
+@onready var coins := %Coins
 
 
-func add_point():
+func _ready() -> void:
+	total_coins = coins.get_child_count()
+
+
+func add_point() -> void:
 	score += 1
-	score_label.text = "Collected\n" + str(score) + " souls"
-	print(score)
+	score_label.text = "Collected\n" + str(score) + "/" + str(total_coins) + " souls"
+	print(score, "/", total_coins)
