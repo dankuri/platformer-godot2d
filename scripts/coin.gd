b09a5da6005445059ef29d487968324007c4f780
dankuri <danek.il420@gmail.com>
2025-04-14 01:12:48 +0500

    slime here broo

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,14 +3,14 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("coin bro")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: Node2D) -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_body_entered(_body: Node2D) -> void:
 	print("+1 coin")
 	queue_free()
