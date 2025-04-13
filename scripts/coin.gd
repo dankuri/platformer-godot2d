c6a182faeab404d32d6ff234865494cc834f4277
dankuri <danek.il420@gmail.com>
2025-04-14 02:03:54 +0500

    text + score + music + sfx

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,16 +1,9 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+@onready var game_manager = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("+1 coin")
-	queue_free()
+	game_manager.add_point()
+	animation_player.play("pickup")
